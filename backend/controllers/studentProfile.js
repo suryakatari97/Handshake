19939ea8704bf3ec1beb184c821a50d981e758f8
suryakatari97 <surya.katari@sjsu.edu>
2020-02-18 20:55:28 -0800

    student experience

--- backend/controllers/studentProfile.js
+++ backend/controllers/studentProfile.js
@@ -1,14 +1,110 @@
-const dbConnection = require('./dbConnectionPool');
+const dbConnection = require('./dbconnection');
 
-var studentdetails = async(studentId,student) => {
+var studentdetails = async (studentId, student) => {
     let conn;
     let msg;
     let status = false;
     let table = 'student_details';
     try {
         conn = await dbConnection();
-        if(conn) {
-            var user = await profileExists(studentId, table, conn)
+        if (conn) {
+            var user = await profileExists(studentId, table, conn);
+            await conn.query("START TRANSACTION");
+            if (!user) {
+                await conn.query('INSERT INTO ?? SET student_id = ?', [table, studentId]);
+            }
+            await conn.query('UPDATE ?? SET ? where student_id = ?', [table, student, studentId]);
+            await conn.query("COMMIT");
+            status = true;
+            msg = "student details updated";
+            console.log(msg);
+        }
+    }
+    catch (e) {
+        console.log(e);
+        msg = "Error in connecting to db";
+        status = false;
+    }
+    finally {
+        if (conn) {
+            await conn.release();
+            await conn.destroy();
+        }
+        return {
+            status: status,
+            message: msg
         }
     }
 }
+
+var studentExperience = async (studentExp) => {
+    let conn;
+    let msg;
+    let status = false;
+    let table = 'student_experience';
+
+    try {
+        conn = await dbConnection();
+        if (conn) {
+            var user = await expprofileexists(table, studentExp, conn);
+            console.log(user);
+            await conn.query("START TRANSACTION");
+            if (!user) {
+                console.log("experience record does not exist ");
+                await conn.query('INSERT INTO ?? SET ?', [table, studentExp]);
+            } else {
+                console.log("experience record exists ");
+                await conn.query('UPDATE ?? SET  title=?, location=?,end_date=? where student_id =? AND company_name=? AND start_date = ?',
+                    [table, studentExp.title, studentExp.location, studentExp.end_date,
+                        studentExp.student_id, studentExp.company_name, studentExp.start_date]);
+            }
+            await conn.query("COMMIT");
+            status = true;
+            msg = "student experience updated";
+            console.log(msg);
+        }
+    } catch (e) {
+        console.log(e);
+        msg = "Error in connecting to db";
+        status = false;
+    }
+    finally {
+        if (conn) {
+            await conn.release();
+            await conn.destroy();
+        }
+        return {
+            status: status,
+            message: msg
+        }
+    }
+}
+
+let profileExists = async (id, table, conn) => {
+
+    console.log(`Id is ${id} searching in table: ${table}`);
+    if (conn) {
+        result = await conn.query('Select * from ?? where student_id = ?', [table, id]);
+        console.log(result);
+        if (result.length > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+let expprofileexists = async (table, studentExp, conn) => {
+    if (conn) {
+        let result = await conn.query('select * from ?? where student_id = ? AND company_name =? AND start_date =?',
+            [table, studentExp.student_id, studentExp.company_name, studentExp.start_date]);
+        console.log(result.length);
+        if (result.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+module.exports = { studentdetails, studentExperience }
--- backend/routes/student_routes.js
+++ backend/routes/student_routes.js
@@ -5,24 +5,67 @@ const router = express.Router();
 const student = require('../controllers/studentProfile');
 
 router.post("/studentdetails", async (req, res) => {
-    var resObj ={};
+    let resObj = {};
     console.log("in student details");
     let studentId = req.body.id;
-    var student = {
-        "name":req.body.name,
+    var studentdet = {
+        "name": req.body.name,
         "dob": req.body.dob,
         "city": req.body.city,
         "state": req.body.state,
         "country": req.body.country,
-        "career_obj": req.body.careerObj 
+        "career_obj": req.body.careerObj
     }
     try {
         console.log("sending data to student_details");
-        resObj = await student.
-        
+        resObj = await student.studentdetails(studentId, studentdet);
+    } catch (e) {
+        console.log(e);
+        resObj.status = false;
+        resObj.message = "Unknown error at database";
     }
+    finally {
+        res.status(200).json(
+            {
+                ...resObj
+            });
+    }
+});
 
-})
+router.post("/studentExperience", async (req, res) => {
+    let resObj = {};
+    console.log("in student experience");
+    if (!req.body.id || !req.body.company_name || !req.body.start_date) {
+        console.log("missing primary key");
+        res.status(400).json({
+            msg: "missing primary key "
+        });
+        return;
+    }
+    var studentExp = {
+        "student_id": req.body.id,
+        "company_name": req.body.company_name,
+        "title": req.body.title,
+        "location": req.body.location,
+        "start_date": req.body.start_date,
+        "end_date": req.body.work_desc
+    }
+    try {
+        console.log("student experience details");
+        resObj = await student.studentExperience(studentExp);
+
+    } catch (e) {
+        console.log(e);
+        resObj.status = false;
+        resObj.message = "error at database";
+    }
+    finally {
+        res.status(200).json(
+            {
+                ...resObj
+            });
+    }
+});
 
 
 
